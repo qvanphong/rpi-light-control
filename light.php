38bdc5e02266a31351154fd98df124f14a213c73
qvanphong <qvanphong@gmail.com>
2019-12-22 22:27:55 +0700

    added content to write

--- light.php
+++ light.php
@@ -8,10 +8,13 @@ if (isset($_POST['light']) && $_POST['light'] == "on") {
     echo "<pre>$output</pre>";
 } else if (isset($_POST['h-on']) && isset($_POST['m-on'])  && isset($_POST['h-off'])  && isset($_POST['m-off'])) {
     $post = $_POST;
+    $folder_location = getcwd() . '/';
     $file = fopen("crontab.txt", "w");
-    $content = $post['h-on'] . " " . $post['m-on'] . " " . $post['h-off'] . " " . $post['m-off'] . " www-data ";
+    $content = $post['h-on'] . " " . $post['m-on'] . " * * *  www-data python " . $folder_location . ' led_on.py' . "\n" .
+        $post['h-off'] . " " . $post['m-off'] . " * * *  www-data python " . $folder_location . ' led_off.py' . "\n";
+
+    fwrite($file, $content);
 
     fclose($file);
 }
-echo getcwd();
 echo "Command executed!";
