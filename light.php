00add0b098c4b2d8d94147a3bf5ebf1fecb02e9f
qvanphong <qvanphong@gmail.com>
2019-12-22 22:20:34 +0700

    commit to rasp to get project folder

--- light.php
+++ light.php
@@ -7,9 +7,11 @@ if (isset($_POST['light']) && $_POST['light'] == "on") {
     $output =  shell_exec("crontab -l 2>&1");
     echo "<pre>$output</pre>";
 } else if (isset($_POST['h-on']) && isset($_POST['m-on'])  && isset($_POST['h-off'])  && isset($_POST['m-off'])) {
+    $post = $_POST;
     $file = fopen("crontab.txt", "w");
+    $content = $post['h-on'] . " " . $post['m-on'] . " " . $post['h-off'] . " " . $post['m-off'] . " www-data ";
 
     fclose($file);
 }
-
+echo getcwd();
 echo "Command executed!";
