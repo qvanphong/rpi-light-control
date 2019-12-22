fdd7c49279577e0b131f74ac0fa5486c879bd03d
qvanphong <qvanphong@gmail.com>
2019-12-22 22:08:37 +0700

    added schedule

--- light.php
+++ light.php
@@ -6,6 +6,10 @@ if (isset($_POST['light']) && $_POST['light'] == "on") {
 } else if (isset($_POST['get_'])) {
     $output =  shell_exec("crontab -l 2>&1");
     echo "<pre>$output</pre>";
+} else if (isset($_POST['h-on']) && isset($_POST['m-on'])  && isset($_POST['h-off'])  && isset($_POST['m-off'])) {
+    $file = fopen("crontab.txt", "w");
+
+    fclose($file);
 }
 
 echo "Command executed!";
